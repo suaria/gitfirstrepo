61586b9fe9be561d749276bfdeffa83c7738bab9
unknown <ariasuhrab@gmail.com>
2024-07-10 07:55:15 -0400

    first commit

---
+++ src/main/java/practice1/E1.java
@@ -0,0 +1,17 @@
+package practice1;public class E1 {
+
+    void firstMethod(int a,int b){
+        if(a>b){
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/practice1/E1Tester.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public class E1Tester {
+    public static void main(String[] args) {
+        E1 task1=new E1();
+        task1.firstMethod(6,8);
+    }
+}
